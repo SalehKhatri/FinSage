b8306de79f22dd5d42c1bd376f3f7d437ccd0d5c
Saleh <salehkhatri29@gmail.com>
2024-03-23 18:18:56 +0530

    CSS fix

--- src/components/ExpenseChart/ExpenseChart.jsx
+++ src/components/ExpenseChart/ExpenseChart.jsx
@@ -28,26 +28,6 @@ function ExpenseChart() {
         setLoading(false);
       });
   };
-  function formatDate(date){
-    var dd = date.getDate();
-    var mm = date.getMonth()+1;
-    var yyyy = date.getFullYear();
-    if(dd<10) {dd='0'+dd}
-    if(mm<10) {mm='0'+mm}
-    date = mm+'/'+dd;
-    return date
- }
-
-function Last7Days () {
-    var result = [];
-    for (var i=0; i<7; i++) {
-        var d = new Date();
-        d.setDate(d.getDate() - i);
-        result.push( formatDate(d) )
-    }
-
-    return(result);
- }
 
   useEffect(() => {
     getWeeklyExpense();
